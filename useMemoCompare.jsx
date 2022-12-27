c0aed153354ebce14c2a70da28a8d764ffb591f8
Mauricio Andrades <mod.and.op@gmail.com>
2022-12-27 00:11:14 -0800

    Update useMemoCompare.jsx

--- useMemoCompare.jsx
+++ useMemoCompare.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useMemo } from "react";
+import React, { useEffect, useRef } from "react";
 
 /**
  * Optimized version of useMemoCompare
- * @see https://usehooks.com/useMemoCompare/
- * This is a custom React hook for providing a behavior similar to the useMemo
+ *
+ * @template {null} T
+ * @param {T} next
+ * @param {(prev: ?T, next: ?T) => boolean} compare
+ * @see https://usehooks.com/useMemoCompare/ This is a custom React hook for providing a behavior similar to the useMemo
  * hook, but with a custom comparison function. It takes in two arguments: next
  * and compare. The hook uses a useRef hook to store the previous value. It then
  * passes the previous and next value to the compare function to determine if
@@ -13,23 +16,21 @@ import React, { useState, useMemo } from "react";
  * value if they are considered equal according to the compare function. This
  * allows the hook to return the same value if the compare function keeps
  * returning true, just like the useMemo hook.
- * @template T
- * @param {T} next
- * @param {(prev: ?T, next: ?T)=>boolean} compare
  */
 function useMemoCompare(next, compare) {
-  // Use useState to store previous value
-  const [ previous, setPrevious ] = useState(null);
-  // Use useMemo to memoize the result of compare
-  const isEqual = useMemo(() => compare(previous, next), [ compare, previous, next ]); // If not equal, update previous value with next value
-  
+  // Ref for storing previous value
+  const previousRef = useRef(null);
+
+// check whether the values of previous and next are both null or both not null or compare.
+  const isEqual = (previousRef.current == null) !== (next == null) ? false : compare(previousRef.current, next);
+
   useEffect(() => {
     if (!isEqual) {
-      setPrevious(next);
+      previousRef.current = next;
     }
   }, [ isEqual, next ]);
   // Finally, if equal then return the previous value
-  return isEqual ? previous : next;
+  return isEqual ? previousRef.current : next;
 }
 
 export default useMemoCompare;
