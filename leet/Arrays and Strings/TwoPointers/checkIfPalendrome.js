3e5b4720a605f72a164354465d67d00f5ab70232
Mauricio Andrades <mod.and.op@gmail.com>
2023-02-28 15:57:15 -0800

    checkIfPalendrome code and test

---
+++ leet/Arrays and Strings/TwoPointers/checkIfPalendrome.js
@@ -0,0 +1,17 @@
+export const checkIfPalendrome = (s) => {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (/^\s*$/.test(s[left])) left++;
+    if (/^\s*$/.test(s[right])) right--;
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
+
+// console.log(checkIfPalendrome('racecar'));
---
+++ leet/Arrays and Strings/TwoPointers/checkIfPalendrome.test.js
@@ -0,0 +1,7 @@
+import { checkIfPalendrome } from "./checkIfPalendrome";
+
+test("checks if a string is a palindrome", () => {
+  expect(checkIfPalendrome("racecar")).toBe(true);
+  expect(checkIfPalendrome("hello")).toBe(false);
+  expect(checkIfPalendrome("A man a plan a canal Panama")).toBe(true);
+});
