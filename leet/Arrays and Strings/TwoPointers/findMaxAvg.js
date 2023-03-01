f2b47d2c467311a4716fc5c735f2d050526f45c7
Mauricio Andrades <mod.and.op@gmail.com>
2023-02-28 17:25:48 -0800

    update findMaxAvg

--- leet/Arrays and Strings/TwoPointers/findMaxAvg.js
+++ leet/Arrays and Strings/TwoPointers/findMaxAvg.js
@@ -37,3 +37,27 @@ export const findMaxAverage = (nums, k) => {
 
     return maxSum/k
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+export const findMaxAvgIter = (nums, k) => {
+    let sum = 0;
+
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    let maxAvg = sum / k;
+
+    for (let i = k; i < nums.length; i++) {
+        sum += nums[i];
+        sum -= nums[i - k];
+
+        maxAvg = Math.max(maxAvg, sum / k);
+    }
+
+    return maxAvg;
+};
--- leet/Arrays and Strings/TwoPointers/findMaxAvg.test.js
+++ leet/Arrays and Strings/TwoPointers/findMaxAvg.test.js
@@ -1,4 +1,4 @@
-import { findMaxAverage } from './findMaxAvg';
+import { findMaxAverage, findMaxAvgIter } from './findMaxAvg';
 
 describe('findMaxAverage', () => {
   it('returns the maximum average of a contiguous subarray of length k', () => {
@@ -7,6 +7,14 @@ describe('findMaxAverage', () => {
     expect(findMaxAverage([-1], 1)).toBe(-1);
     expect(findMaxAverage([1, 2, 3, 4, 5], 1)).toBe(5);
     expect(findMaxAverage([1, 2, 3, 4, 5], 5)).toBe(3);
+  });
+
+  it('returns the maximum average of a contiguous subarray of length k using an iterative for loop', () => {
+    expect(findMaxAvgIter([1, 12, -5, -6, 50, 3], 4)).toBe(12.75);
+    expect(findMaxAvgIter([5], 1)).toBe(5);
+    expect(findMaxAvgIter([-1], 1)).toBe(-1);
+    expect(findMaxAvgIter([1, 2, 3, 4, 5], 1)).toBe(5);
+    expect(findMaxAvgIter([1, 2, 3, 4, 5], 5)).toBe(3);
   });
 });
 
